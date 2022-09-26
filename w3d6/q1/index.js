c2dd425563e227ce557ce006b2bdd001a9cff9e1
Melaku G.Denbel <44544124+melaku0077@users.noreply.github.com>
2022-09-26 04:40:22 -0400

    MVC assignment

---
+++ w3d6/q1/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "view"));
+
+app.get("/", (req, res) => {
+  const date = new Date();
+  res.render("index", {
+    time: date.toTimeString(),
+  });
+});
+app.listen(8000);
